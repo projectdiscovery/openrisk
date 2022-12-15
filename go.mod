575a907508afca2d80d3cfca5746cb0b8bb49377
Doğan Can Bakır <dogancanbakir@protonmail.com>
2022-12-15 12:14:56 +0300

    init

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/projectdiscovery/openrisk
+
+go 1.18
+
+require (
+	github.com/dsnet/compress v0.0.1 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
+	github.com/mholt/archiver v3.1.1+incompatible // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/nwaples/rardecode v1.1.0 // indirect
+	github.com/pierrec/lz4 v2.6.0+incompatible // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/projectdiscovery/gologger v1.1.5 // indirect
+	github.com/sashabaranov/go-gpt3 v0.0.0-20221202105456-0f9f4aa343ad // indirect
+	github.com/ulikunitz/xz v0.5.7 // indirect
+	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
+	gopkg.in/djherbis/times.v1 v1.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/projectdiscovery/gologger"
+	gogpt "github.com/sashabaranov/go-gpt3"
+)
+
+var banner = fmt.Sprintf(`
+experimental  
+____  ____  ___  ____  _____(_)____/ /__
+/ __ \/ __ \/ _ \/ __ \/ ___/ / ___/ //_/
+/ /_/ / /_/ /  __/ / / / /  / (__  ) ,<   
+\____/ .___/\___/_/ /_/_/  /_/____/_/|_|  Powered by OpenAI (GPT-3)
+  /_/                                   
+  `)
+
+func printBanner() {
+	gologger.Print().Msgf("%s\n", banner)
+	gologger.Print().Msgf("\t\tprojectdiscovery.io\n\n")
+}
+
+const Question = "Calculate the 10-scale risk score for the following Nuclei scan result"
+
+var input = flag.String("i", "", "Nuclei scan result file path")
+
+func main() {
+	printBanner()
+	flag.Parse()
+
+	if *input == "" {
+		flag.PrintDefaults()
+		return
+	}
+
+	bNucleiScanResult, err := os.ReadFile(*input)
+	if err != nil {
+		gologger.Error().Msgf("Could not read the nuclei scan result.", err)
+		return
+	}
+	nucleiScanResult := string(bNucleiScanResult)
+	var prompt = buildPrompt(nucleiScanResult)
+	var completion = getCompletion(prompt)
+	gologger.Info().Label("RISK SCORE").Msg(completion)
+}
+
+func getCompletion(prompt string) string {
+	fmt.Println(prompt)
+	var apiKey = os.Getenv("OPENAI_API_KEY")
+	c := gogpt.NewClient(apiKey)
+	ctx := context.Background()
+
+	req := gogpt.CompletionRequest{
+		Model:            "text-davinci-003",
+		Temperature:      0.01, // FIXME: https://github.com/sashabaranov/go-gpt3/issues/9
+		TopP:             1,
+		FrequencyPenalty: 0.01,
+		PresencePenalty:  0.01,
+		BestOf:           1,
+		MaxTokens:        256,
+		Prompt:           prompt,
+	}
+	resp, err := c.CreateCompletion(ctx, req)
+	if err != nil {
+		gologger.Error().Msgf("An error occurred while getting the completion.", err)
+		return ""
+	}
+	return strings.TrimSpace(resp.Choices[0].Text)
+}
+
+func buildPrompt(nucleiScanResult string) string {
+	var sb strings.Builder
+	sb.WriteString(Question)
+	sb.WriteString("\n")
+	sb.WriteString(nucleiScanResult)
+	return sb.String()
+}
